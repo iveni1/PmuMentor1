0fcd0c59427dfe87d6e99283d8864314194df986
iveni1 <168824571+iveni1@users.noreply.github.com>
2024-05-03 23:26:15 +0300

    Update UserService.java add login.
    we add login in the code

--- UserService.java
+++ UserService.java
@@ -7,5 +7,10 @@ public class UserService {
 
     public void createUser(String username, String password, String email) {
         users.put(username, new User(username, password, email));
+    }
+        
+    public boolean login(String username, String password) {
+        User user = users.get(username);
+        return user != null && user.getPassword().equals(password);
   }
 }
