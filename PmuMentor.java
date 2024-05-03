41224dcfccdf0cf6b8ac4331852b94f32a65e407
iveni1 <168824571+iveni1@users.noreply.github.com>
2024-05-03 23:45:25 +0300

    Create PmuMentor.java

---
+++ PmuMentor.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class PmuMentor {
+    public static void main(String[] args) {
+        UserService userService = new UserService();
+        Scanner scanner = new Scanner(System.in);
+    }
+}
