ddc752457cb88940e34db7d9674f9f921ad60ddf
iveni1 <168824571+iveni1@users.noreply.github.com>
2024-05-03 23:20:03 +0300

    Update UserService.java add CreateUser.
    we add a code for create user.

--- UserService.java
+++ UserService.java
@@ -3,6 +3,9 @@ import java.util.Map;
 import java.util.Collection;
 
 public class UserService {
-    
+    protected Map<String, User> users = new HashMap<>();
+
+    public void createUser(String username, String password, String email) {
+        users.put(username, new User(username, password, email));
   }
 }
